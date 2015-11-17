5754e5144bf064a37a32e02475a2151726ce3009
Kirill Vilkov <wide.wrd@gmail.com>
2015-11-17 19:19:24 +0300

    wip

--- Strategy/RoadMap.cs
+++ Strategy/RoadMap.cs
@@ -3,14 +3,55 @@ using Com.CodeGame.CodeRacing2015.DevKit.CSharpCgdk.Model;
 
 namespace Com.CodeGame.CodeRacing2015.DevKit.CSharpCgdk
 {
-    class RoadMap
-    {
-        public class Tile
-        {
-            private TileType type;
-            private int tileX;
-            private int tileY;
+    class RoadMap {
+		
+        public class Tile {
+            public TileType type = TileType.Unknown;
+			private RoadMap roadMap;
+
+			public int posX { get; private set; }
+			public int posY { get; private set; }
+
+			Tile(RoadMap roadMap, int posX, int posY) {
+				this.posX = posX;
+				this.posY = posY;
+				this.roadMap = roadMap;
+			}
+
+			bool canGoInDirection(AxisDirection dir) {
+				
+			}
+
         }
+
+		public int width { get; private set; }
+		public int height { get; private set; }
+
+		private Tile[,] tiles;
+
+		RoadMap(int width, int height) {
+			this.width = width;
+			this.height = height;
+
+			tiles = new Tile[this.width, this.height];
+			for (int x = 0; x < width; ++x) {
+				for (int y = 0; y < height; ++y) {
+					tiles[x, y] = new Tile(x, y); 
+				}
+			}
+		}
+
+		void updateMap(TileType[,] map) {
+			for (int x = 0; x < width; ++x) {
+				for (int y = 0; y < height; ++y) {
+					tiles[x, y].type = map[x, y];
+				}
+			}
+		}
+
+		Tile tileAt(int x, int y) {
+			return tiles[x, y];
+		}
 
     }
 }
--- Strategy/Vector.cs
+++ Strategy/Vector.cs
@@ -5,35 +5,87 @@ using System.Text;
 
 namespace Com.CodeGame.CodeRacing2015.DevKit.CSharpCgdk
 {
-    public struct Vector
-    {
-        public double x;
-        public double y;
+	public enum AxisDirection {
+		up, down, left, right
+	}
 
-        Vector(double _x, double _y)
-        {
+    public struct Vector {
+		
+		public double x { get; private set; }
+		public double y { get; private set; }
+
+        Vector(double _x, double _y) {
             x = _x;
             y = _y;
         }
+
+		Vector(AxisDirection dir) {
+			switch (dir) {
+			case AxisDirection.down:
+				this = Vector.down;
+				break;
+			case AxisDirection.left:
+				this = Vector.left;
+				break;
+			case AxisDirection.right:
+				this = Vector.right;
+				break;
+			case AxisDirection.up:
+				this = Vector.up;
+				break;
+			}
+		}
+
+		public AxisDirection direction {
+			get {
+				if (this * Vector.up >= 0.5) return AxisDirection.up;
+				if (this * Vector.down >= 0.5) return AxisDirection.down;
+				if (this * Vector.left >= 0.5) return AxisDirection.left;
+				return AxisDirection.right;
+			}
+		}
       
-        public static Vector operator + (Vector a, Vector b)
-        {
+        public static Vector operator + (Vector a, Vector b) {
             return new Vector(a.x + b.x, a.y + b.y);
         }
 
-        public static Vector operator - (Vector a, Vector b)
-        {
+        public static Vector operator - (Vector a, Vector b) {
             return new Vector(a.x - b.x, a.y - b.y);
         }
 
-        public static Vector operator * (Vector a, double value)
-        {
+        public static Vector operator * (Vector a, double value) {
             return new Vector(a.x * value, a.y * value);
         }
 
-        public static double operator * (Vector a, Vector b)
-        {
+		public static Vector operator / (Vector a, double value) {
+			return new Vector(a.x / value, a.y / value);
+		}
+
+        public static double operator * (Vector a, Vector b) {
             return (a.x * b.x) + (a.y * b.y);
         }
+
+		public double length {
+			get {
+				return Math.Sqrt(x * x + y * y);
+			}
+		}
+
+		public Vector normalized {
+			get {
+				return this / this.length;
+			}
+		}
+
+		public static Vector up = new Vector(0, -1);
+		public static Vector down = new Vector(0, 1);
+		public static Vector left = new Vector(-1, 0);
+		public static Vector right = new Vector(1, 0);
+
+		public static bool operator == (Vector a, Vector b) {
+			const double E = 0.0001;
+
+			return Math.Abs(a.x - b.x) < E && Math.Abs(a.y - b.y) < E;
+		}
     }
 }
